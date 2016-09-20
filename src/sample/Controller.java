1c062bc27a89d6660b14260c994c09b2f7217d29
elisalter <esal284@aucklanduni.ac.nz>
2016-09-20 19:01:49 +1200

    changes to Controller.java and sample.fxml

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
 public class Controller {
+
+    public void newSpellingQuizClicked(){
+        System.out.println("User clicked new spelling quiz...");
+    }
+
+    public void reviewQuizClicked(){
+        System.out.println("User clicked review quiz...");
+    }
+
+    public void viewStatisticsClicked(){
+        System.out.println("User clicked view statistics...");
+    }
+
+    public void clearStatisticsClicked(){
+        System.out.println("User clicked clear statistics...");
+    }
+
 }
