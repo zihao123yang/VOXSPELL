dc5a79b84892e95d8954ef3be27df412773ad825
elisalter <esal284@aucklanduni.ac.nz>
2016-09-22 23:11:48 +1200

    added ViewStats.fxml

--- src/sample/ClearStatisticsScene.java
+++ src/sample/ClearStatisticsScene.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.net.URL;
@@ -21,25 +22,50 @@ public class ClearStatisticsScene implements Initializable{
     @FXML
     Label noLabel;
 
+    @FXML
+    Button mainMenuButton;
+
+    @FXML
+    Button yesButton;
+
+    @FXML
+    Button noButton;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         yesLabel.setVisible(false);
         noLabel.setVisible(false);
+        mainMenuButton.setVisible(false);
+        mainMenuButton.disarm();
     }
 
 
 
     public void yesPressed(){
         yesLabel.setVisible(true);
+
         _db.clearStats();
         //_rv.clearFailed();
+
+        yesButton.setVisible(false);
+        noButton.setVisible(false);
+
+        mainMenuButton.arm();
+        mainMenuButton.setVisible(true);
 
 
 
     }
 
     public void noPressed(){
+
         noLabel.setVisible(true);
+        mainMenuButton.arm();
+
+        yesButton.setVisible(false);
+        noButton.setVisible(false);
+
+        mainMenuButton.setVisible(true);
 
     }
 
