61535235388cd8de0ac6c158be8f492b53b2a0c3
zihao123yang <zihao123yang@gmail.com>
2016-09-22 20:42:38 +1200

    festival calls working using scm file, working on changing voice video working spelling logic working working on revision logic
    stats to do
    review to do
    gui sometimes doesnt show
    fix up

---
+++ src/sample/DisplayingStats.java
@@ -0,0 +1,41 @@
+package sample;
+
+/**
+ * Created by zihao123yang on 22/09/16.
+ */
+public class DisplayingStats {
+
+    /**
+     * returns a string representation of the users stats, to be displayed in the view stats scene.
+     * @param levelToDisplay
+     * @return
+     */
+    public String printStatistics(int levelToDisplay){
+
+        StringBuilder sb = new StringBuilder();
+
+        int timesAppeared;
+
+        for(Word word : _db.getStoredStats()){
+
+            // if the level the user specifies is equal to the level of the current word
+            if(word.getLevel() == levelToDisplay){
+
+                timesAppeared = word.getNumMastered() + word.getNumFaulted() + word.getNumFailed();
+
+                sb.append(word.toString() + "\n");
+                sb.append("\n");
+                sb.append("    Times appeared: " + timesAppeared + "\n");
+                sb.append("    Times mastered: " + word.getNumMastered() + "\n");
+                sb.append("    Times faulted:  " + word.getNumFaulted() + "\n");
+                sb.append("    Times failed:   " + word.getNumFailed() + "\n");
+                sb.append("\n");
+
+            }
+
+        }
+
+        return sb.toString();
+
+    }
+}
--- src/sample/Statistics.java
+++ src/sample/Statistics.java
@@ -14,14 +14,14 @@ public class Statistics {
     int _wordsTested;
     DataBase _db;
 
-    //public Statistics(int numWords, DataBase db) {
-    //    _numWords = numWords;
-    //    _mastered = 0;
-    //    _faulted = 0;
-    //    _failed = 0;
-    //    _wordsTested = 0;
-    //    _db = db;
-    //}
+    public Statistics(int numWords, DataBase db) {
+        _numWords = numWords;
+        _mastered = 0;
+        _faulted = 0;
+        _failed = 0;
+        _wordsTested = 0;
+        _db = db;
+    }
 
     public void increaseMastered() {
         _mastered++;
@@ -48,40 +48,6 @@ public class Statistics {
         } else {
             return false;
         }
-
-    }
-
-    /**
-     * returns a string representation of the users stats, to be displayed in the view stats scene.
-     * @param levelToDisplay
-     * @return
-     */
-    public String printStatistics(int levelToDisplay){
-
-        StringBuilder sb = new StringBuilder();
-
-        int timesAppeared;
-
-        for(Word word : _db.getStoredStats()){
-
-            // if the level the user specifies is equal to the level of the current word
-            if(word.getLevel() == levelToDisplay){
-
-                timesAppeared = word.getNumMastered() + word.getNumFaulted() + word.getNumFailed();
-
-                sb.append(word.toString() + "\n");
-                sb.append("\n");
-                sb.append("    Times appeared: " + timesAppeared + "\n");
-                sb.append("    Times mastered: " + word.getNumMastered() + "\n");
-                sb.append("    Times faulted:  " + word.getNumFaulted() + "\n");
-                sb.append("    Times failed:   " + word.getNumFailed() + "\n");
-                sb.append("\n");
-
-            }
-
-        }
-
-        return sb.toString();
 
     }
 }
